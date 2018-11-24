4667f10daed5547d71f891c60d568b1fd539134a
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-24 21:36:07 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    array["mangos"] = "mangos and"
     array.join(", ")
+    array["mangos"] = "mangos and"
 end
 end
