36e8cb21834a53f7c99b7c242cc55d9f32e2f12d
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-24 21:21:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-    last_element = array[-1]
-    last_element["and "]
+    array[-1] = " and"
     array.join(", ")
   
 end
