a80f1e444c9932c32afdb53cc091f65763b8d59e
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-24 21:11:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,6 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2
     array.join(" and ")
-    binding.pry
   elsif array.length >= 3
     array[-1].append "and "
     array.join(", ")
