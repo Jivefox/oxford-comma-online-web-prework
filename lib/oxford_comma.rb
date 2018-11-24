3c5e505c17cba63757ebfb9baaa20b1be4243f4a
Cliff Jurgensen <jivefox.002@gmail.com>
2018-11-24 21:32:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,9 @@
-require 'pry'
-
 def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
     array.join(" and ")
   elsif array.length >= 3
-  binding.pry
     array[-2].prepend 'and '
     array.join(", ")
 end
